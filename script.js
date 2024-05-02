e2246302a78a48e9cead788518ace510bf061405
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-05-02 21:57:23 +0530

    submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
+let list = document.getElementById("infi-list")
 
+
+for(let i=1;i<11;i++){
+    list.innerHTML+=`
+    <li>Item ${i}</li>
+    `
+}
+let count=10
+
+list.addEventListener("scroll",()=>{
+    if(list.scrollHeight - list.scrollTop - list.clientHeight < 1){
+ 
+        list.innerHTML += `
+        <li>Item ${count+1}</li>
+        <li>Item ${count+2}</li>
+        `
+        count=count+2;
+      }
+})
